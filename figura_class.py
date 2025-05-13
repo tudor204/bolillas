d8a0b85e0c7bf1f6c1292136b559df2464ee13fd
Tudor <tudor204@hotmail.com>
2025-05-13 13:26:52 +0200

    clases

---
+++ figura_class.py
@@ -0,0 +1,28 @@
+class Rectangulo:
+    def __init__(self):
+            print("Hola esto es rectangulo")
+
+object=Rectangulo()
+"""
+tamaño (800,600)
+
+x=0
+vx=1
+y=300
+vy=1
+
+x2=780
+vx2=1
+y2=300
+vy2=1
+
+x += vx
+    y += vy 
+    if x >= 800 or x==0:#los limites en x
+        vx *= -1
+    if y >= 600 or y==0:#los limites en y
+        vy *= -1
+
+
+
+"""
---
+++ refresh_pygame.py
@@ -0,0 +1,11 @@
+import pygame as pg
+
+pg.init()
+
+#definir pantalla
+pantalla= pg.display.set_mode(())
+
+
+
+
+pg.quit()
